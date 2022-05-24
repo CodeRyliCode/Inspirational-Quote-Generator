111f06a574a7d42608aa941e05e2d3b77a71cb5b
Ryli Davis <rylidavis94@gmail.com>
2022-05-23 22:02:39 -0700

    Creating an array of objects for quotes & sources

--- js/script.js
+++ js/script.js
@@ -10,6 +10,31 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+//This is an array of objects
+let quotes = [
+
+{
+   quote: "Do not judge me by my success, judge me by how many times I fell down and got back up again.",
+  source: "Nelson Mandela"
+},
+{
+  quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+  source: "Mother Teresa"
+},
+{
+  quote: "It is during our darkest moments that we must focus to see the light.",
+  source: "Aristotle"
+},
+{
+  quote: "Do not let making a living prevent you from making a life.",
+  source: "John Wooden"
+},
+{
+  quote: "Love the life you live. Live the life you love.",
+  source: "Bob Marley"
+}
+
+];
 
 
 
