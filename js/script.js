921c48280322cf70f843ee58771a3dffdc7b3f2f
Ryli Davis <rylidavis94@gmail.com>
2022-06-09 13:45:33 -0700

    Changed default quote & added random bg colors

--- js/script.js
+++ js/script.js
@@ -65,9 +65,6 @@ return randomObject;
 getRandomQuote();
 
 
-
-
-
 /***
  * `printQuote` function
 ***/
@@ -123,6 +120,18 @@ document.getElementById("quote-box").innerHTML = quoteElements;
 
 
 }
+
+function random_bg_color() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+console.log(bgColor);
+
+  document.body.style.background = bgColor;
+  }
+
+random_bg_color();
 
 
 
